1481b6c7e94ac0380705cb07a3cd5a3cf65e88c1
Ilham Firdaus <ucuncs89@gmail.com>
2023-09-22 15:08:49 +0700

    feat history history

---
+++ src/modules/project/controllers/project_history.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  UseGuards,
+  Req,
+  Query,
+  Put,
+} from '@nestjs/common';
+import { ProjectService } from '../services/project.service';
+import { CreateProjectDto } from '../dto/create-project.dto';
+import { UpdateProjectDto } from '../dto/update-project.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/modules/auth/jwt-auth.guard';
+import { I18n, I18nContext } from 'nestjs-i18n';
+import { GetListProjectDto } from '../dto/get-list-project.dto';
+import { Pagination } from 'src/utils/pagination';
+import { GetListProjectHistoryDto } from '../dto/get-list-project-history.dto';
+import { ProjectHistoryService } from '../services/project_history.service';
+
+@ApiBearerAuth()
+@ApiTags('project')
+@UseGuards(JwtAuthGuard)
+@Controller('project')
+export class ProjectHistoryController {
+  constructor(private readonly projectHistoryService: ProjectHistoryService) {}
+
+  @Get(':id/history')
+  async findAll(
+    @Query() query: GetListProjectHistoryDto,
+    @Param('id') id: number,
+    @I18n() i18n: I18nContext,
+  ) {
+    const _page = query.page || 1;
+    const _page_size = query.page_size || 10;
+    const data = await this.projectHistoryService.findAll(
+      {
+        page: (_page - 1) * _page_size,
+        page_size: _page_size,
+        sort_by: query.sort_by || 'created_at',
+        order_by: query.order_by || 'DESC',
+      },
+      id,
+    );
+    const pagination = await Pagination.pagination(
+      data.total_data,
+      _page,
+      _page_size,
+      `/project/${id}`,
+    );
+    return { message: 'Successfully', data, pagination };
+  }
+}
