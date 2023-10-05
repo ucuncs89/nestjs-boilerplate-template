4cfeb242eb52e7cf0ce9d5068054a9c5ca6b7ba1
Ilham Firdaus <ucuncs89@gmail.com>
2023-10-05 12:08:37 +0700

    feat: add vendor material fabric, sewing, packaging

---
+++ src/modules/project/dto/project-vendor-material.dto.ts
@@ -0,0 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { ProjectVendorMaterialFabricDto } from './project-vendor-material-fabric.dto';
+import { IsNotEmpty, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { ProjectVendorMaterialSewingDto } from './project-vendor-material-sewing.dto';
+import { ProjectVendorMaterialPackagingDto } from './project-vendor-material-packaging.dto';
+
+export class ProjectVendorMaterialDto {
+  @ApiProperty({
+    isArray: true,
+    type: ProjectVendorMaterialFabricDto,
+  })
+  @ValidateNested({ each: true })
+  @Type(() => ProjectVendorMaterialFabricDto)
+  vendor_fabric?: ProjectVendorMaterialFabricDto[];
+
+  @ApiProperty({
+    isArray: true,
+    type: ProjectVendorMaterialSewingDto,
+  })
+  @ValidateNested({ each: true })
+  @Type(() => ProjectVendorMaterialSewingDto)
+  vendor_accessories_sewing?: ProjectVendorMaterialSewingDto[];
+
+  @ApiProperty({
+    isArray: true,
+    type: ProjectVendorMaterialPackagingDto,
+  })
+  @ValidateNested({ each: true })
+  @Type(() => ProjectVendorMaterialPackagingDto)
+  vendor_accessories_packaging?: ProjectVendorMaterialPackagingDto[];
+}
