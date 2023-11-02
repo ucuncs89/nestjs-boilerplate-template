87041268add19e657e7bcc482e9e98b120670e62
Ilham Firdaus <ucuncs89@gmail.com>
2023-11-02 09:22:06 +0700

    feat: roles asign validation

--- src/modules/roles/roles.module.ts
+++ src/modules/roles/roles.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RolesEntity } from '../../entities/roles/roles.entity';
 import { RolesController } from './controllers/roles.controller';
 import { RolesService } from './services/roles.service';
+import { RolesPermissionGuard } from './roles-permission';
+import { UsersRolesEntity } from 'src/entities/users/users_roles.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RolesEntity])],
+  imports: [TypeOrmModule.forFeature([RolesEntity, UsersRolesEntity])],
   controllers: [RolesController],
-  providers: [RolesService],
+  providers: [RolesService, RolesPermissionGuard],
 })
 export class RolesModule {}
