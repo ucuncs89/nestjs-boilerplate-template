75b1bccf04af7dd21f144d205647fa56905cf40c
Ilham Firdaus <ucuncs89@gmail.com>
2024-03-19 12:51:01 +0700

    fix total cost

--- src/modules/project/planning/dto/project-planning-sampling.dto.ts
+++ src/modules/project/planning/dto/project-planning-sampling.dto.ts
@@ -1,9 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class ProjectPlanningSamplingDto {
   @ApiProperty()
   name: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   total_cost: number;
 }
